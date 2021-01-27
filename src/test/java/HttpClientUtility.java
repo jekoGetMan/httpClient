c4788dd5188c591b8c3e7e4828c75182fe25fe95
Yevhen_Pavlenko <Yevhen.Pavlenko@refinitiv.com>
2021-01-27 09:57:58 +0200

    Updated HttpClient

--- src/test/java/HttpClientUtility.java
+++ src/test/java/HttpClientUtility.java
@@ -2,12 +2,15 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public final class HttpClientUtility {
     public static final String baseURI = "https://reqres.in/";
@@ -16,16 +19,20 @@ public final class HttpClientUtility {
     private HttpClientUtility() {
     }
 
-    public static String sendGet(String endpoint) throws IOException {
-        HttpGet httpGet = new HttpGet(baseURI + endpoint);
+    public static String sendGet(String endpoint) throws IOException, URISyntaxException {
+        URI uri = new URIBuilder(baseURI + endpoint).setParameter("page", "2").build();
+        HttpGet httpGet = new HttpGet(uri);
+
         return getResponse(client.execute(httpGet));
     }
 
-    public static String sendPost(String endpoint, String jsonBody) throws IOException {
-        HttpPost httpPost = new HttpPost(baseURI + endpoint);
+    public static String sendPost(String endpoint, String jsonBody) throws IOException, URISyntaxException {
+        URI uri = new URIBuilder(baseURI + endpoint).build();
+        HttpPost httpPost = new HttpPost(uri);
         httpPost.setHeader("content-type", "application/json");
         StringEntity stringEntity = new StringEntity(jsonBody);
         httpPost.setEntity(stringEntity);
+
         return getResponse(client.execute(httpPost));
     }
 
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         String jsonBody = "{\n" + "    \"name\": \"morpheus\",\n" + "    \"job\": \"leader\"\n" + "}";
 
         System.out.println("********************************************************");
-        System.out.println(HttpClientUtility.sendGet("/api/users?page=2"));
+        System.out.println(HttpClientUtility.sendGet("/api/users"));
         System.out.println("********************************************************");
         System.out.println(HttpClientUtility.sendPost("api/users", jsonBody));
         System.out.println("********************************************************");
